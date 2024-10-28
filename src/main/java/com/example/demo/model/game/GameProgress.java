a6333b12e1ce33f7fcb1d6da46231eaacf531584
hiddenSharp429 <z404878860@163.com>
2024-10-28 22:32:59 +0800

    ✨ Add 'Auth' modules && 'Websocket' modules
    Add 'Auth' modules && 'Websocket' modules
    
    (cherry picked from commit 33d54a4fb39a7bd3d2a33d2af273527798619521)

---
+++ src/main/java/com/example/demo/model/game/GameProgress.java
@@ -0,0 +1,29 @@
+/*
+ * @Author: hiddenSharp429 z404878860@163.com
+ * @Date: 2024-10-28 20:03:08
+ * @LastEditors: hiddenSharp429 z404878860@163.com
+ * @LastEditTime: 2024-10-28 20:03:23
+ */
+package com.example.demo.model;
+
+public class GameProgress {
+    private String playerId;
+    private double percentage;
+
+    // Getters and Setters
+    public String getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(String playerId) {
+        this.playerId = playerId;
+    }
+
+    public double getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(double percentage) {
+        this.percentage = percentage;
+    }
+}

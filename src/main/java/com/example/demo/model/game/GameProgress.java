caa7a71cb7c4314edd152b1145236886a9fb1d0c
hiddenSharp429 <z404878860@163.com>
2024-11-27 21:12:17 +0800

    🎨 Optimize the game progress

--- src/main/java/com/example/demo/model/game/GameProgress.java
+++ src/main/java/com/example/demo/model/game/GameProgress.java
@@ -8,10 +8,27 @@ package com.example.demo.model.game;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class GameProgress {
+    private String type;
     private String playerId;
     private double percentage;
+    private Stats stats;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Stats {
+        private double wpm;
+        private double accuracy;
+        private int errorCount;
+        private String username;
+    }
 }

f43a9a9874d0da6a13a45a65719bc9a2ce3666d6
hiddenSharp429 <z404878860@163.com>
2025-01-03 10:54:50 +0800

    🐛 Fix swagger bug

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,18 +1,19 @@
 /*
  * @Author: hiddenSharp429 z404878860@163.com
  * @Date: 2024-10-27 18:35:36
- * @LastEditors: hiddenSharp429 z404878860@163.com
- * @LastEditTime: 2024-10-27 18:36:11
  */
 package com.example.demo.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnProperty(name = "springdoc.api-docs.enabled", havingValue = "true", matchIfMissing = true)
 public class OpenApiConfig {
 
     @Bean
